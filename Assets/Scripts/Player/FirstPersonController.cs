82d320bd5b017b50998ebeed3d0b6b8923edbca5
Jared Schwartz <jschwartz12@elon.edu>
2018-04-23 17:22:40 +0200

    small changes

--- Assets/Scripts/Player/FirstPersonController.cs
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -60,7 +60,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             // the jump state needs to read here to make sure it is not missed
             if (!m_Jump)
             {
-				m_Jump = Input.GetKeyDown(KeyCode.J);
+				m_Jump = Input.GetButtonDown ("Jump");
             }
 
             if (!m_PreviouslyGrounded && m_CharacterController.isGrounded)
