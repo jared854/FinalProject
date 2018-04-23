41dae3a82885e3c6518ee23dcf869731eb8faa7a
Gray Kline <gkline@middlebury.edu>
2018-04-23 09:34:00 +0200

    note panel stuff

---
+++ Assets/Scripts/NotePanel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotePanel : MonoBehaviour {
+
+	public GameObject Panel1;
+	public GameObject Panel2;
+
+	void Start ()
+	{
+		Panel1.gameObject.SetActive (true);
+		Panel2.gameObject.SetActive (false);
+	}
+
+	public void hidePanel1()
+	{
+		Panel1.gameObject.SetActive (false);
+		Panel2.gameObject.SetActive (true);
+	}
+
+	public void hidePanel2()
+	{
+		Panel2.gameObject.SetActive (false);
+		Panel1.gameObject.SetActive (true);
+	}
+}
