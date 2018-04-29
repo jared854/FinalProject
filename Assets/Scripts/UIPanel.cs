b4e7126321b38f7ee446efc8870f22cdd11cb0df
Gray Kline <gkline@middlebury.edu>
2018-04-29 15:47:16 +0200

    Implemented a points UI, including a total points display and a slider for progression in each level. Still needs to be hooked up with the specific enemies and scenes. To do this, when an enemy is killed, call UpdatePoints(addPoints) with the number of points gained (or lost) as the parameter. Also, call UpdateSlider(maxPoints, points) with the num of points needed to successfully complete the level as the first parameter, and the num of points gained or lost as the second paramter. While the total points should persist throughout the game, ClearSlider() should be called at the beginning of each level in order to reset the slider.

--- Assets/Scripts/UIPanel.cs
+++ Assets/Scripts/UIPanel.cs
@@ -1,16 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
 
 public class UIPanel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	[SerializeField] RectTransform pointSlider;
+	[SerializeField] Text pointsText;
+
+	public static int totalPoints = 0;
+
+	void Start(){
+		UpdatePoints (0);
+		UpdateSlider (1, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void UpdateSlider(int maxPoints, int points){
+		if (pointSlider == null)
+			return;
+		float relativeScale = (float)points / (float)maxPoints;
+		Vector3 scale = pointSlider.transform.localScale;
+		scale.x = relativeScale;
+		pointSlider.transform.localScale = scale;
+	}
+
+	void UpdatePoints(int addPoints){
+		if (pointsText == null)
+			return;
+		string format = "D2"; // D2 means decimal format with a minimum of 2 digits, for one digit numbers this will give a preceding 0;
+		// int newTotal = Int32.Parse(pointsText.text);
+		totalPoints += addPoints;
+		pointsText.text = totalPoints.ToString (format);
+	}
+
+	void ClearSlider(){
+		UpdateSlider (1, 0);
 	}
 }
