080343f871957a4003e20c044a05ac1abd869e1f
Jared Schwartz <jschwartz12@elon.edu>
2018-04-30 10:55:52 +0200

    Added menu, control sceen, and buttons

---
+++ Assets/Scripts/SceneSwitching.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitching : MonoBehaviour {
+
+	void Update(){
+
+	}
+
+	public void LoadControlsScene(){
+		SceneManager.LoadScene ("ControlsScene");
+	}
+
+	public void LoadFirstNotesScene(){
+		SceneManager.LoadScene ("Note_scene_1");
+	}
+
+	public void LoadMainMenuScene(){
+		SceneManager.LoadScene ("MainMenu");
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+		Debug.Log ("Quitting Game...");
+	}
+}
